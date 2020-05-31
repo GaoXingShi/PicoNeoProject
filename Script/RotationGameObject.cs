84920dcf642158018bd0e9bb942267f6808215ea
gaoxingshi <gaoxingshi@58.com>
2020-05-31 21:17:16 +0800

    chore : 更改场景，有色魔方有位移有旋转。

---
+++ Script/RotationGameObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationGameObject : MonoBehaviour
+{
+    // 是否是顺时针
+    public bool isSequence = true;
+    private const float ANGLE = 0.5f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(isSequence ? (Vector3.up) : -Vector3.up * ANGLE);
+    }
+}
---
+++ Script/TranslateGameObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslateGameObject : MonoBehaviour
+{
+    public int count = 0;
+    private const float SPEED = 0.01f;
+    private Vector3 currentDirect = -Vector3.forward;
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = transform.position + currentDirect * SPEED;
+        //transform.Translate(currentDirect * SPEED);
+        count++;
+        if (count == 180)
+        {
+            count = 0;
+            currentDirect = -currentDirect;
+        }
+    }
+}
