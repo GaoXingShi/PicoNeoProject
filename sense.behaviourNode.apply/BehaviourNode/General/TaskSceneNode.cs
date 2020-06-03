8573098eba86780e633edbf4cd0b1100a30bdf7e
GaoXingShi <113456881@qq.com>
2020-06-04 00:48:15 +0800

    chore : 完善场景内容。

---
+++ sense.behaviourNode.apply/BehaviourNode/General/TaskSceneNode.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Sense.BehaviourTree.VRTKExtend
+{
+    public class TaskSceneNode : BehaviourNode
+    {
+        public string sceneName;
+        public override void Execute()
+        {
+            State = NodeState.Succeed;
+            SceneManager.LoadScene(sceneName);
+        }
+
+        public override void ResetNode()
+        {
+            base.ResetNode();
+        }
+
+        public override void Abort(NodeState _state)
+        {
+            base.Abort(_state);
+        }
+    }
+}
